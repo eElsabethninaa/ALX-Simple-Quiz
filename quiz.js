fdb2a22566d4171573280a3876657f62858f138f
Elsabeth <143882270+eElsabethninaa@users.noreply.github.com>
2025-08-01 18:42:29 +0300

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer() {
+    //Function body
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[name ="quiz"]: checked').value;
+    const feedback = document.getElementById("feedback");
+
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+
+    const submitButton = document.getElementById("submit-answer");
+
+    submitButton.addEventListener("click", checkAnswer);
+};
